168e9fb24e1987a33a8b80349d9848977a92e01e
sabdulmajid <ayman.hasib@outlook.com>
2021-06-28 19:02:15 +0300

    Adjusted code

--- src/index.js
+++ src/index.js
@@ -129,11 +129,9 @@ return null;
 var button = document.createElement("button");
 button.innerHTML = "Play Again";
 
-// 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
 
-// 3. Add event handler
 button.addEventListener ("click", function() {
   window.location.reload(false);
 });
