9221c3f1bbeb08b5b630fc1adc77963917d589a7
sabdulmajid <ayman.hasib@outlook.com>
2021-06-28 14:18:51 +0300

    Changes to the game mechanics

--- src/index.js
+++ src/index.js
@@ -20,13 +20,20 @@ class Square extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
     }
     
     renderSquare(i) {
@@ -39,8 +46,16 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner && winner != 'draw') {
+        status = 'Winner: ' + winner;
+      } else if (winner && winner === 'draw') {
+        status = `Draw! Try again!`;
+      } else {
+        status = `Next player: ${(this.state.xIsNext ? 'X' : 'O')}`;
+      }
+    
       return (
         <div>
           <div className="status">{status}</div>
@@ -86,3 +101,39 @@ class Square extends React.Component {
     <Game />,
     document.getElementById('root')
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+    }
+    else if(!squares.includes(null)){
+        return 'draw';
+    }
+}
+return null;
+}
+
+var button = document.createElement("button");
+button.innerHTML = "Play Again";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  window.location.reload(false);
+});
